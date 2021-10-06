67c2228f17ee44cc9b48402d75f0b2b3848ee214
Rahul avaghan <rahul.avaghan@coachhub.io>
2021-10-06 08:41:38 +0200

    largest-component-improvement

--- graph/traversal/largest-component.js
+++ graph/traversal/largest-component.js
@@ -1,32 +1,42 @@
 
+/***
+ * 
+ * 
+ *  1 -> 
+ * 
+ * 
+ * 
+ * 
+ */
 
-largestComponent = (graph) =>{
+
+
+ largestComponent = (graph) =>{
     const count = {value: 0}
     let largestNodes = 0;
     const visited = new Set();
   
     for(const current in graph){
-       explore(graph,current, count, visited)
-      if(count.value > largestNodes){
-        largestNodes = count.value;
+       const size = explore(graph,current, visited)
+      if(size > largestNodes){
+        largestNodes = size;
       }
-      count.value = 0;
     }
     console.log(largestNodes)
   }
   
   
-  explore = (graph,current, count, visited) => {
+  explore = (graph,current, visited) => {
   
-    if(visited.has(String(current))) return false;
+    if(visited.has(String(current))) return 0;
   
     visited.add(String(current));
+    let size= 1;
     for(const neighbor of graph[current]){
-      explore(graph,neighbor, count, visited);
+      size += explore(graph,neighbor, visited);
     }
   
-    count.value +=1;
-    
+    return size;
   }
   
   largestComponent({
@@ -38,3 +48,35 @@ largestComponent = (graph) =>{
     3: [2, 4],
     4: [3, 2]
   }) 
+  
+  
+  largestComponent({
+    1: [2],
+    2: [1,8],
+    6: [7],
+    9: [8],
+    7: [6, 8],
+    8: [9, 7, 2]
+  })
+  //  -> 6
+  largestComponent({
+    3: [],
+    4: [6],
+    6: [4, 5, 7, 8],
+    8: [6],
+    7: [6],
+    5: [6],
+    1: [2],
+    2: [1]
+  })//5
+  
+  largestComponent({
+    0: [4,7],
+    1: [],
+    2: [],
+    3: [6],
+    4: [0],
+    6: [3],
+    7: [0],
+    8: []
+  })//3
